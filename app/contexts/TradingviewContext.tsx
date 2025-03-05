ca9c57ed2359bfee5ae386a2e68df5acfe603010
zcelbir <zcelbir@proven.com.tr>
2025-03-05 10:15:05 +0300

    add order history

--- app/contexts/TradingviewContext.tsx
+++ app/contexts/TradingviewContext.tsx
@@ -1,37 +1,55 @@
-import { widget, type Bar, type IChartingLibraryWidget, type ResolutionString } from "public/tradingview/charting_library";
+import {
+  widget,
+  type Bar,
+  type EntityId,
+  type IChartingLibraryWidget,
+  type ResolutionString,
+} from "public/tradingview/charting_library";
 import React, { createContext, useContext, useState, useEffect } from "react";
 import { createDataFeed } from "~/routes/chart/data/customDataFeed";
 import { useWebSocketContext } from "./WebSocketContext";
 import { useWsObserver } from "~/hooks/useWsObserver";
 import { processWSCandleMessage } from "~/routes/chart/data/processChartData";
 import { useTradeDataStore } from "~/stores/TradeDataStore";
+import {
+  calculateDistance,
+  circleFillData,
+  createCircle,
+  mapResolutionToMiliseconds,
+} from "~/routes/chart/utils";
 
 interface TradingViewContextType {
   chart: IChartingLibraryWidget | null;
 }
 
-const TradingViewContext = createContext<TradingViewContextType>({ chart: null });
+const TradingViewContext = createContext<TradingViewContextType>({
+  chart: null,
+});
 
 export interface ChartContainerProps {
-    symbolName: string;
-    interval: ResolutionString;
-    libraryPath: string;
-    chartsStorageUrl: string;
-    chartsStorageApiVersion: string;
-    clientId: string;
-    userId: string;
-    fullscreen: boolean;
-    autosize: boolean;
-    studiesOverrides: any;
-    container: string;
-  }
+  symbolName: string;
+  interval: ResolutionString;
+  libraryPath: string;
+  chartsStorageUrl: string;
+  chartsStorageApiVersion: string;
+  clientId: string;
+  userId: string;
+  fullscreen: boolean;
+  autosize: boolean;
+  studiesOverrides: any;
+  container: string;
+}
 
-export const TradingViewProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+export const TradingViewProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
   const [chart, setChart] = useState<IChartingLibraryWidget | null>(null);
+  const [periodAsSeconds, setPeriodAsSeconds] = useState<number | null>(null);
+  const [orderHistoryShapes, setOrderHistoryShapes] =
+    useState<Array<EntityId | null> | null>(null);
 
-  const { subscribe} = useWsObserver();
-  const {symbol} = useTradeDataStore();
-
+  const { subscribe } = useWsObserver();
+  const { symbol } = useTradeDataStore();
 
   const defaultProps: Omit<ChartContainerProps, "container"> = {
     symbolName: "BTC",
@@ -46,21 +64,21 @@ export const TradingViewProvider: React.FC<{ children: React.ReactNode }> = ({ c
     studiesOverrides: {},
   };
 
-      // const changeSubscription = (payload: any) => {
-      //   subscribe('candle', 
-      //     {payload: payload,
-      //     handler: candleDataHandler,
-      //     single: true
-      //   })
-      // }
-  
-      // useEffect(() => {
-      //   changeSubscription({
-      //     coin: defaultProps.symbolName,
-      //     // interval: defaultProps.interval,
-      //     interval: "1d",
-      //   });
-      // }, [defaultProps.symbolName])
+  // const changeSubscription = (payload: any) => {
+  //   subscribe('candle',
+  //     {payload: payload,
+  //     handler: candleDataHandler,
+  //     single: true
+  //   })
+  // }
+
+  // useEffect(() => {
+  //   changeSubscription({
+  //     coin: defaultProps.symbolName,
+  //     // interval: defaultProps.interval,
+  //     interval: "1d",
+  //   });
+  // }, [defaultProps.symbolName])
 
   useEffect(() => {
     const tvWidget = new widget({
@@ -82,22 +100,92 @@ export const TradingViewProvider: React.FC<{ children: React.ReactNode }> = ({ c
       loading_screen: { backgroundColor: "#0e0e14" },
       // load_last_chart:false,
       time_frames: [
-        { text: "1m", resolution: "1" as ResolutionString},   
-        { text: "5m", resolution: "5" as ResolutionString},   
-        { text: "15m", resolution: "15" as ResolutionString}, 
-        { text: "1H", resolution: "60" as ResolutionString},  
-        { text: "4H", resolution: "240" as ResolutionString}, 
-        { text: "1D", resolution: "1D" as ResolutionString },  
-
-    ],
-    });    
+        { text: "1m", resolution: "1" as ResolutionString },
+        { text: "5m", resolution: "5" as ResolutionString },
+        { text: "15m", resolution: "15" as ResolutionString },
+        { text: "1H", resolution: "60" as ResolutionString },
+        { text: "4H", resolution: "240" as ResolutionString },
+        { text: "1D", resolution: "1D" as ResolutionString },
+      ],
+    });
 
     setChart(tvWidget);
 
     return () => tvWidget.remove();
   }, [symbol]);
 
-  return <TradingViewContext.Provider value={{ chart }}>{children}</TradingViewContext.Provider>;
+  useEffect(() => {
+    if (chart) {
+      chart.onChartReady(() => {
+        const widged = chart.chart();
+
+        const periodAsSeconds = mapResolutionToMiliseconds(
+          chart.activeChart().resolution()
+        );
+
+        setPeriodAsSeconds(periodAsSeconds);
+
+        const circleShapeArr: Array<EntityId | null> = [];
+
+        circleFillData.forEach((circleData) => {
+          const circle = createCircle(
+            widged,
+            circleData.time,
+            circleData.price,
+            circleData.color,
+            circleData.size,
+            periodAsSeconds
+          );
+
+          circleShapeArr.push(circle);
+        });
+
+        setOrderHistoryShapes(circleShapeArr);
+
+        // if (circleShapeArr.length > 0) {
+        //   circleShapeArr.forEach((circle) => {
+        //     if (circle) {
+        //       const activeShape = chart.activeChart().getShapeById(circle);
+
+        //       activeShape.setProperties({
+        //         color: "transparent",
+        //       });
+        //     }
+        //   });
+        // }
+      });
+    }
+  }, [chart]);
+
+  useEffect(() => {
+    if (chart && orderHistoryShapes && orderHistoryShapes.length > 0) {
+      chart.onChartReady(() => {
+        chart
+          .activeChart()
+          .crossHairMoved()
+          .subscribe(null, ({ offsetX, offsetY }) => {
+            orderHistoryShapes.forEach((order) => {
+              if (order && offsetX && offsetY) {
+                const activeShape = chart.activeChart().getShapeById(order);
+
+                calculateDistance(
+                  offsetX,
+                  offsetY,
+                  chart.activeChart(),
+                  activeShape
+                );
+              }
+            });
+          });
+      });
+    }
+  }, [chart, orderHistoryShapes]);
+
+  return (
+    <TradingViewContext.Provider value={{ chart }}>
+      {children}
+    </TradingViewContext.Provider>
+  );
 };
 
 export const useTradingView = () => useContext(TradingViewContext);
--- app/routes/chart/utils.ts
+++ app/routes/chart/utils.ts
@@ -1,22 +1,112 @@
+import type {
+  EntityId,
+  IChartingLibraryWidget,
+  IChartWidgetApi,
+  ILineDataSourceApi,
+} from "public/tradingview/charting_library/charting_library";
+
 export const mapResolutionToInterval = (resolution: string): string => {
-    const mapping: Record<string, string> = {
-      "1": "1m",
-      "5": "5m",
-      "15": "15m",
-      "60": "1h",
-      "240": "4h",
-      "D": "1d",
-      "W": "1w",
-    };
-    return mapping[resolution] || "1d";
+  const mapping: Record<string, string> = {
+    "1": "1m",
+    "5": "5m",
+    "15": "15m",
+    "60": "1h",
+    "240": "4h",
+    D: "1d",
+    W: "1w",
   };
-  
+  return mapping[resolution] || "1d";
+};
 
+export const mapResolutionToMiliseconds = (resolution: string): number => {
+  const mapping: Record<string, number> = {
+    "1": 60,
+    "5": 300,
+    "15": 900,
+    "60": 3600,
+    "240": 14400,
+    D: 86400,
+    W: 604800,
+  };
+  return mapping[resolution] || 86400;
+};
 
-  export function resolutionToSeconds(resolution: string): number {
-    if (resolution === "1D") return 86400;
-    if (resolution === "W") return 604800;
-    if (resolution === "M") return 2592000;
+export function resolutionToSeconds(resolution: string): number {
+  if (resolution === "1D") return 86400;
+  if (resolution === "W") return 604800;
+  if (resolution === "M") return 2592000;
 
-    return Number(resolution);
-  }
+  return Number(resolution);
+}
+
+export function createCircle(
+  chart: IChartWidgetApi,
+  time: number,
+  price: number,
+  color: string,
+  diameter: number,
+  periodAsSeconds: number
+): EntityId | null {
+  const shape = chart.createMultipointShape(
+    [
+      { time: time, price: price },
+      { time: time + periodAsSeconds * diameter, price: price },
+    ],
+    {
+      shape: "circle",
+      overrides: {
+        backgroundColor: color,
+        linewidth: 0,
+        color: "transparent",
+      },
+      disableSelection: true, // Allow selection
+      disableSave: true, // Do not save to cloud
+      lock: true,
+      zOrder: "top",
+    }
+  );
+
+  return shape;
+}
+
+export function calculateDistance(
+  offsetX: number,
+  offsetY: number,
+  chart: IChartWidgetApi,
+  activeShape: ILineDataSourceApi
+) {
+  const activeShapePoints = activeShape.getPoints();
+  const diff = activeShapePoints[1].time - activeShapePoints[0].time;
+
+  const timeScale = chart.getTimeScale();
+
+  const ratio = chart.getPriceToBarRatio();
+
+  console.log(ratio, diff, timeScale.barSpacing())
+
+  // const timeCenter = activeShapePoints[0].time;
+  // const diameterPoint = activeShapePoints[1].time;
+
+  // const diameterAsPixel = chart.getTimeScale().g  timeCenter - diameterPoint
+
+  // const xAxisDiameter =
+  //   offsetX > timeCenter - diameterAsPixel &&
+  //   offsetX < timeCenter + diameterAsPixel;
+  // const yAxisDiameter =
+  //   offsetY > priceCenter - diameterAsPixel &&
+  //   offsetX < priceCenter + diameterAsPixel;
+
+  // console.log(xAxisDiameter, yAxisDiameter);
+
+  // return xAxisDiameter && yAxisDiameter;
+}
+
+const sellColor = "rgb(239, 83, 80, 0.3)";
+const buyColor = "rgba(95, 255, 242, 0.3)";
+
+export const circleFillData = [
+  { time: 1739623705, price: 86000, color: sellColor, size: 3 },
+  { time: 1739191705, price: 90000, color: buyColor, size: 3.5 },
+  { time: 1740036941, price: 102000, color: buyColor, size: 5 },
+  { time: 1738481741, price: 106000, color: sellColor, size: 4 },
+];
