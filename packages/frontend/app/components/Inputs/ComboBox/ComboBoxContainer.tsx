2d1781126bad957a20a97338c406b48667b6c557
tcoskun <tcoskun@proven.com.tr>
2025-08-18 15:20:30 +0300

    fixed : disappear y axis when enable debug wallet

--- packages/frontend/app/components/Inputs/ComboBox/ComboBoxContainer.tsx
+++ packages/frontend/app/components/Inputs/ComboBox/ComboBoxContainer.tsx
@@ -7,6 +7,7 @@ import { useUserDataStore } from '~/stores/UserDataStore';
 import { debugWallets, wsEnvironments, wsUrls } from '~/utils/Constants';
 import ComboBox from './ComboBox';
 import styles from './ComboBox.module.css';
+import { HorizontalScrollable } from '~/components/Wrappers/HorizontanScrollable/HorizontalScrollable';
 export default function ComboBoxContainer() {
     const { symbol, selectedCurrency, setSelectedCurrency } =
         useTradeDataStore();
@@ -43,118 +44,139 @@ export default function ComboBoxContainer() {
 
     return (
         <section className={styles.comboBoxContainers}>
-            <div className={styles.wsUrlSelector}>
-                {sdkEnabled ? (
+            <div id={'debug-wallet-static-area'} className={styles.itemWrapper}>
+                <div className={styles.wsUrlSelector}>
+                    {sdkEnabled ? (
+                        <ComboBox
+                            value={wsEnvironment}
+                            options={wsEnvironments}
+                            fieldName='value'
+                            onChange={(value) => setWsEnvironment(value)}
+                        />
+                    ) : (
+                        <ComboBox
+                            value={wsUrl}
+                            options={wsUrls}
+                            onChange={(value) => setWsUrl(value)}
+                        />
+                    )}
+                </div>
+                <div className={styles.currencySelector}>
                     <ComboBox
-                        value={wsEnvironment}
-                        options={wsEnvironments}
-                        fieldName='value'
-                        onChange={(value) => setWsEnvironment(value)}
+                        value={selectedCurrency}
+                        options={currencies}
+                        onChange={(value) => setSelectedCurrency(value)}
                     />
-                ) : (
-                    <ComboBox
-                        value={wsUrl}
-                        options={wsUrls}
-                        onChange={(value) => setWsUrl(value)}
-                    />
-                )}
-            </div>
-            <div className={styles.currencySelector}>
-                <ComboBox
-                    value={selectedCurrency}
-                    options={currencies}
-                    onChange={(value) => setSelectedCurrency(value)}
-                />
-            </div>
-            <div className={styles.divider} />
-            <div
-                className={`${styles.wsToggle} ${isWsEnabled ? styles.wsToggleRunning : styles.wsTogglePaused}`}
-                onClick={() => setIsWsEnabled(!isWsEnabled)}
-            >
-                <div className={styles.wsToggleButton}>
-                    {' '}
-                    {isWsEnabled ? 'WS' : 'WS'}
                 </div>
+                <div className={styles.divider} />
             </div>
-            <div
-                className={`${styles.wsToggle} ${isWsSleepMode ? styles.wsToggleRunning : styles.wsTogglePaused}`}
-                onClick={() => setIsWsSleepMode(!isWsSleepMode)}
+            <HorizontalScrollable
+                excludes={['debug-wallet-static-area']}
+                wrapperId='trade-page-left-section'
+                offset={20}
             >
-                <div className={styles.wsToggleButton}>
-                    {isWsSleepMode ? 'WS Sleep' : 'WS Sleep'}
-                </div>
-            </div>
-            <div
-                className={`${styles.sdkToggle} ${sdkEnabled ? styles.active : styles.disabled}`}
-                onClick={() => setSdkEnabled(!sdkEnabled)}
-            >
-                <div className={styles.sdkToggleButton}>
-                    {sdkEnabled ? 'SDK' : 'SDK'}
-                </div>
-            </div>
-            <div
-                className={`${styles.sdkToggle} ${usePythOracle ? styles.active : styles.disabled}`}
-                onClick={() => setUsePythOracle(!usePythOracle)}
-            >
-                <div className={styles.sdkToggleButton}>
-                    {usePythOracle ? 'Pyth' : 'HL'} Oracle
-                </div>
-            </div>
+                <div className={styles.itemWrapper}>
+                    <div
+                        className={`${styles.wsToggle} ${isWsEnabled ? styles.wsToggleRunning : styles.wsTogglePaused}`}
+                        onClick={() => setIsWsEnabled(!isWsEnabled)}
+                    >
+                        <div className={styles.wsToggleButton}>
+                            {' '}
+                            {isWsEnabled ? 'WS' : 'WS'}
+                        </div>
+                    </div>
+                    <div
+                        className={`${styles.wsToggle} ${isWsSleepMode ? styles.wsToggleRunning : styles.wsTogglePaused}`}
+                        onClick={() => setIsWsSleepMode(!isWsSleepMode)}
+                    >
+                        <div className={styles.wsToggleButton}>
+                            {isWsSleepMode ? 'WS Sleep' : 'WS Sleep'}
+                        </div>
+                    </div>
+                    <div
+                        className={`${styles.sdkToggle} ${sdkEnabled ? styles.active : styles.disabled}`}
+                        onClick={() => setSdkEnabled(!sdkEnabled)}
+                    >
+                        <div className={styles.sdkToggleButton}>
+                            {sdkEnabled ? 'SDK' : 'SDK'}
+                        </div>
+                    </div>
+                    <div
+                        className={`${styles.sdkToggle} ${usePythOracle ? styles.active : styles.disabled}`}
+                        onClick={() => setUsePythOracle(!usePythOracle)}
+                    >
+                        <div className={styles.sdkToggleButton}>
+                            {usePythOracle ? 'Pyth' : 'HL'} Oracle
+                        </div>
+                    </div>
 
-            <div className={styles.divider} />
-            <div
-                className={`${styles.walletSelector} ${!isDebugWalletActive ? styles.passive : ' '}`}
-            >
-                <ComboBox
-                    value={debugWallet.label}
-                    options={debugWallets}
-                    fieldName='label'
-                    onChange={(value) =>
-                        setDebugWallet({
-                            label: value,
-                            address:
-                                debugWallets.find(
-                                    (wallet) => wallet.label === value,
-                                )?.address || '',
-                        })
-                    }
-                />
-            </div>
-            <div
-                className={`${styles.sdkToggle} ${isDebugWalletActive ? styles.active : styles.disabled}`}
-                onClick={() => setIsDebugWalletActive(!isDebugWalletActive)}
-            >
-                <div className={styles.sdkToggleButton}>
-                    {isDebugWalletActive ? 'Debug Wallet' : 'Sessions Wallet'}
-                </div>
-            </div>
-            <div className={styles.subInfo}>{userAddress}</div>
-
-            <div className={styles.divider} />
-            {isEstablished(sessionState) && (
-                <button
-                    onClick={() => {
-                        (async () => {
-                            if (isEstablished(sessionState)) {
-                                console.log('established');
-                                const userWalletKey =
-                                    sessionState.walletPublicKey ||
-                                    sessionState.sessionPublicKey;
-                                const ix = instructions.pingIx(42n, {
-                                    actor: sessionState.sessionPublicKey,
-                                    target: userWalletKey,
-                                });
-                                console.log({ ix, sessionState });
-                                const result =
-                                    await sessionState.sendTransaction([ix]);
-                                console.log({ result });
+                    <div className={styles.divider} />
+                    <div
+                        className={`${styles.walletSelector} ${!isDebugWalletActive ? styles.passive : ' '}`}
+                    >
+                        <ComboBox
+                            value={debugWallet.label}
+                            options={debugWallets}
+                            fieldName='label'
+                            onChange={(value) =>
+                                setDebugWallet({
+                                    label: value,
+                                    address:
+                                        debugWallets.find(
+                                            (wallet) => wallet.label === value,
+                                        )?.address || '',
+                                })
                             }
-                        })();
-                    }}
-                >
-                    Ping
-                </button>
-            )}
+                        />
+                    </div>
+                    <div
+                        className={`${styles.sdkToggle} ${isDebugWalletActive ? styles.active : styles.disabled}`}
+                        onClick={() =>
+                            setIsDebugWalletActive(!isDebugWalletActive)
+                        }
+                    >
+                        <div className={styles.sdkToggleButton}>
+                            {isDebugWalletActive
+                                ? 'Debug Wallet'
+                                : 'Sessions Wallet'}
+                        </div>
+                    </div>
+                    <div className={styles.subInfo}>{userAddress}</div>
+
+                    <div className={styles.divider} />
+                    {isEstablished(sessionState) && (
+                        <div>
+                            <button
+                                onClick={() => {
+                                    (async () => {
+                                        if (isEstablished(sessionState)) {
+                                            console.log('established');
+                                            const userWalletKey =
+                                                sessionState.walletPublicKey ||
+                                                sessionState.sessionPublicKey;
+                                            const ix = instructions.pingIx(
+                                                42n,
+                                                {
+                                                    actor: sessionState.sessionPublicKey,
+                                                    target: userWalletKey,
+                                                },
+                                            );
+                                            console.log({ ix, sessionState });
+                                            const result =
+                                                await sessionState.sendTransaction(
+                                                    [ix],
+                                                );
+                                            console.log({ result });
+                                        }
+                                    })();
+                                }}
+                            >
+                                Ping
+                            </button>
+                        </div>
+                    )}
+                </div>
+            </HorizontalScrollable>
         </section>
     );
 }
